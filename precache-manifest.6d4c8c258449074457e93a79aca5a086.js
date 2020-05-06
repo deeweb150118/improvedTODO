e1db6bdf55ca7a18041df2d479dc5c2b22f441eb
deepanshu-kav <62233903+deepanshu-kav@users.noreply.github.com>
2020-05-06 17:37:19 +0530

    Updates

---
+++ precache-manifest.6d4c8c258449074457e93a79aca5a086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8728e8d8c19bccdd0ebe48fd4bf694",
+    "url": "/improvedTODO/index.html"
+  },
+  {
+    "revision": "ba4181c4e6b58e46dff7",
+    "url": "/improvedTODO/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0556bffd6ebabbf3573d",
+    "url": "/improvedTODO/static/js/2.9a376947.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/improvedTODO/static/js/2.9a376947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba4181c4e6b58e46dff7",
+    "url": "/improvedTODO/static/js/main.a807f6d9.chunk.js"
+  },
+  {
+    "revision": "410cfbc98958a188f6ec",
+    "url": "/improvedTODO/static/js/runtime-main.6fc4f9ca.js"
+  }
+]);
